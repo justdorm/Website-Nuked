434e227df0d20434488218477ba8cf79d18d759a
Justdorm <justin.dormitzer@gmail.com>
2023-04-26 12:04:53 -0400

    added video

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-import Masonry from "masonry-layout";
-
-
-window.onload = () => {
-    const grid = document.querySelector('.anim-grid');
-    
-    const masonry = new Masonry(grid, {
-        itemSelector: '.grid-item',
-    });
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:5afb5372c82e9dfc246a6d6a35615ebb87c29422be92758372689121b3821614
+size 1271
